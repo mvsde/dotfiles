579dd0876323865202243a869d276d345c39aa7b
Fynn Becker <post@fynn.be>
2019-04-12 19:20:13 +0200

    Update PowerShell profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,9 @@
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+
+Import-Module posh-git
+Import-Module pure-pwsh
+
+New-Alias dc docker-compose
+New-Alias la Get-ChildItem
+New-Alias which Get-Command
