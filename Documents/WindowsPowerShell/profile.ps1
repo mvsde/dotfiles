f1badc8903d97d21e5f7c952114562915270f526
Fynn Becker <post@fynn.be>
2019-04-13 09:51:42 +0200

    Format PowerShell profile

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -1,9 +1,18 @@
+# Disable the annoying bell
 Set-PSReadlineOption -BellStyle None
+
+# Quit terminal with Ctrl+D
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
-Import-Module posh-git
-Import-Module pure-pwsh
+# Use PSReadline for history
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+# Aliases
 New-Alias dc docker-compose
 New-Alias la Get-ChildItem
 New-Alias which Get-Command
+
+# Modules
+Import-Module posh-git
+Import-Module pure-pwsh
