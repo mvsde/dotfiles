09fb1ab5c62d5dbba1fbe1200bfc1dc600632316
Fynn Becker <post@fynn.be>
2023-05-26 11:39:44 +0200

    feat(setup): Add Fedora multimedia codecs

--- setup.sh
+++ setup.sh
@@ -126,3 +126,10 @@ cd $REPO_DIR
 sudo cp $REPO_DIR/services/disable-usb-wakeup.service /lib/systemd/system/disable-usb-wakeup.service
 systemctl enable disable-usb-wakeup.service
 systemctl start disable-usb-wakeup.service
+
+# Multimedia codecs
+# https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
+
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
