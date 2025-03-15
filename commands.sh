52ccdba42eb6b71fff9fffd0ff3d0a2e519c0196
Fynn Ellie Becker <post@fynn.be>
2025-03-15 07:50:09 +0100

    feat(commands): Add Git mirror clone

--- commands.sh
+++ commands.sh
@@ -17,3 +17,11 @@ rm -rf ~/.config/google-chrome/Default/GPUCache/
 # Delete ignored files
 
 find FOLDER_NAME -name '*.git' -execdir sh -c 'cd {}/.. && git clean -f -d -X' \;
+
+# Mirror clone repository
+# https://docs.github.com/en/repositories/creating-and-managing-repositories/duplicating-a-repository#mirroring-a-repository-in-another-location
+
+git clone --mirror [URL]
+git remote add [NAME] [URL]
+git fetch --prune origin
+git push --mirror [NAME]
